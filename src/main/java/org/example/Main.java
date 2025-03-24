557f43cffc504f8d5f48e1ca3d9821eed5aa4583
Anna <anna.zakoveria@gmail.com>
2025-03-24 14:29:14 +0200

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,49 @@
-package org.example;
+import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Підключення до бази даних
+        String url = "jdbc:mysql://localhost:3306/universitydb";
+        String user = "anna";  // користувач
+        String password = "annann12032003";  // пароль
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введіть місяць для пошуку студентів (1-12): ");
+        int month = scanner.nextInt();  // Вводимо номер місяця для пошуку
+
+        // SQL-запит для вибору студентів, народжених у зазначений місяць
+        String query = "SELECT * FROM Students WHERE MONTH(birth_date) = ?";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            // Підставляємо номер місяця в запит
+            statement.setInt(1, month);
+            ResultSet resultSet = statement.executeQuery();
+
+            // Виводимо результати
+            boolean hasResults = false;
+            System.out.println("Студенти, народжені в місяці " + month + ":");
+            while (resultSet.next()) {
+                hasResults = true;
+                int id = resultSet.getInt("ID");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                String middleName = resultSet.getString("middle_name");
+                Date birthDate = resultSet.getDate("birth_date");
+                String studentCardNumber = resultSet.getString("student_card_number");
+
+                System.out.println("ID: " + id + ", Ім'я: " + firstName + " " + lastName + " " + middleName +
+                        ", Дата народження: " + birthDate + ", Номер залікової книжки: " + studentCardNumber);
+            }
+
+            if (!hasResults) {
+                System.out.println("Студентів, народжених у цьому місяці, не знайдено.");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
